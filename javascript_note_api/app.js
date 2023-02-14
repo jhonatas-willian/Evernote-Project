2a838c3685dd69095fe69811c0798d125e942b14
Jhonatas Willian <Jhoow-_@hotmail.com>
2023-02-13 23:16:30 -0300

    Correção porta back-end

--- javascript_note_api/app.js
+++ javascript_note_api/app.js
@@ -9,11 +9,11 @@ var notesRouter = require('./app/routes/notes');
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
