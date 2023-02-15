c2f2f55dcf69f5aff7ea09fb565e0887c27ef5ba
Jhonatas Willian <Jhoow-_@hotmail.com>
2023-02-15 13:12:42 -0300

    ADD: Rotas na API

--- javascript_note_api/app/routes/users.js
+++ javascript_note_api/app/routes/users.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const { reset } = require('nodemon');
 require('dotenv').config();
 const secret = process.env.JWT_TOKEN
+const withAuth = require('../middlewares/auth');
 
 router.post('/register', async(req, res) => {
   const { name, email, password } = req.body;
@@ -37,6 +38,44 @@ router.post('/login', async(req, res) => {
     }
   } catch (error) {
     res.status(500).json({error: 'Internal error, please try again'});
+  }
+});
+
+router.put('/', withAuth, async function(req, res) {
+  const { name, email } = req.body;
+  
+  try {
+    var user = await User.findOneAndUpdate(
+      {_id: req.user._id}, 
+      { $set: { name: name, email: email}}, 
+      { upsert: true, 'new': true }
+    )
+    res.json(user);
+  } catch (error) {
+    res.status(401).json({error: error});
+  }
+});
+
+router.put('/password', withAuth, async function(req, res) {
+    const { password } = req.body;
+
+    try {
+      var user = await User.findOne({_id: req.user._id})
+      user.password = password
+      user.save()
+      res.json(user);
+    } catch (error) {
+      res.status(401).json({error: error});
+    }
+});
+
+router.delete('/', withAuth, async function(req, res) {
+  try {
+    let user = await User.findOne({_id: req.user._id });
+    await user.delete();
+    res.json({message: 'OK'}).status(201);
+  } catch (error) {
+    res.status(500).json({error: error});
   }
 });
 
