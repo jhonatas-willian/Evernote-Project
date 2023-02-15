5640b8e45439815b99a27a88cf8161892412e5c9
Jhonatas Willian <Jhoow-_@hotmail.com>
2023-02-15 13:11:48 -0300

    ADD: User edit, edit_password, delete

--- javascript_note/src/components/header_logged/index.js
+++ javascript_note/src/components/header_logged/index.js
@@ -9,6 +9,7 @@ import { faList } from "@fortawesome/free-solid-svg-icons";
 
 function HeaderLogged(props) {
     const [redirectToHome, setRedirectToHome] = useState(false);
+    const [user, setUser] = useState(localStorage.getItem('user'));
   
     const logOut = async () => {
         await UsersService.logout();
@@ -58,7 +59,7 @@ function HeaderLogged(props) {
                     <Dropdown>
                         <Dropdown.Trigger>
                             <Button className="button" color="white" outlined>
-                                <span>Jhonatas ▼</span>
+                                <span>{JSON.parse(user)['name']} ▼</span>
                             </Button>
                         </Dropdown.Trigger>
                         <Dropdown.Menu>
